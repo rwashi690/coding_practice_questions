5c8115082f77bddf897be2ed91cb011a549397c9
rwashi690 <rwashington325@gmail.com>
2022-01-26 20:30:45 -0700

    Added practice coding questions

---
+++ q3.js
@@ -0,0 +1,8 @@
+// What is node.js
+
+// 1) Explain what a callback function is and provide a simple example
+// 2) How would you check if a number is an integer?
+// 3) Write a function that would allow you to do this?
+// var addSix = createBase(6);
+// addSix(10); // returns 16
+// addSix(21); // returns 27
---
+++ q4.js
@@ -0,0 +1,3 @@
+//Write a function that takes an array of numbers and a function as parameters. 
+// The function paremeter should return true if the input meets a certain condition or false otherwise. 
+// Your function should return true if the function parameter returns true for _any_ of the array elements in the array parameter or false otherwise. 
